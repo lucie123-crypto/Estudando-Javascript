0842fa38f00466b176c21c34209d61a5ce96f126
LUCIEL CARVALHO AGUIAR <carvalholuciel943@gmail.com>
2024-12-12 11:00:33 -0300

    Particularidades

---
+++ script.js
@@ -0,0 +1,17 @@
+// Consoloe no navegador
+console.log("primeiro");
+
+console.log("Ultimo");
+
+// Declara uma variavel
+var a = 1;
+
+
+a = 'teste';
+
+console.log(a);
+
+
+/*
+Este e um comentario de Multiplas linhas em javascript
+*/
