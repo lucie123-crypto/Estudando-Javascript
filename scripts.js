8f0a7489e4688aecab469aeee997288a3e32e951
LUCIEL CARVALHO AGUIAR <carvalholuciel943@gmail.com>
2024-12-12 11:01:36 -0300

    Declarando Variáveis

---
+++ scripts.js
@@ -0,0 +1,40 @@
+var teste =1;
+
+var nome = "João"
+
+console.log("teste")
+
+teste = 'matheus'
+
+console.log(teste)
+
+console.log(nome)
+
+var $nome = 'luciel'
+
+var _nome = 'fabricio'
+
+console.log($nome)
+
+console.log(_nome)
+
+var nome5 = 'thalis'
+
+console.log(nome5)
+
+var meuprimeiroNome = 'Maria'
+
+console.log(meuprimeiroNome) // camelCase
+
+let testando = 1
+
+const ola = 2
+
+console.log(testando)
+console.log(ola)
+
+
+var meunome
+meunome = 'larisa'
+
+console.log (meunome)
